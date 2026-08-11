aa581881e0ba76d0061ae90eb2db7c81bd851157
bhargav0628 <bhargav061028@gmail.com>
2026-08-11 18:28:08 +0530

    Time: 7 ms (5.4%), Space: 10.2 MB (30.33%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string s1; 
+        for(int i = 0;i<s.length();i++){
+             if(isalnum(s[i])){
+                s1+=tolower(s[i]);
+             }
+        }
+        
+         
+         int left = 0;
+        int right = s1.length() - 1;
+        while(left<right){
+            if(s1[left]!=s1[right]){
+                return false;
+            }
+            left++;
+            right--;
+        } 
+        return true;
+        
+    }
+};
