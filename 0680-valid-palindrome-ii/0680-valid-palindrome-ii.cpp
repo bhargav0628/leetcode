c195623a02247442a3b300108cad5611b93a353b
bhargav0628 <bhargav061028@gmail.com>
2026-08-12 16:19:46 +0530

    Time: 8 ms (20.84%), Space: 30 MB (5.81%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool palindrome(string s,int left,int right){
+        while(left<right){
+            if(s[left]!=s[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int left = 0;
+        int right = s.size()-1;
+        bool x,y;
+        if(palindrome(s,left,right)){
+            return true;
+        }
+        while(left<right){
+            if(s[left]!=s[right]){
+                left++;
+               x = palindrome(s,left,right);
+              if(!x){
+                left--;
+                right--;
+                 y = palindrome(s,left,right);
+              }
+              if(!y){
+                return false;
+              }
+            }
+            left++;
+            right--;
+
+        }
+         
+            return true;
+    }
+};
