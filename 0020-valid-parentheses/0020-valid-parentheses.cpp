1de7963f4878d569ce5547462139dfea28afddf8
bhargav0628 <bhargav061028@gmail.com>
2026-08-30 22:27:03 +0530

    Time: 0 ms (100%), Space: 9 MB (37.37%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0;i<s.size();i++){
+            if(!st.empty()&&((s[i]==']'&&st.top()=='[')
+            ||(s[i]=='}'&&st.top()=='{')||
+            (s[i]==')'&&st.top()=='('))){
+                st.pop();
+            } else{
+            st.push(s[i]);
+            }
+        }
+        if(st.empty()){
+            return true;
+        }
+        return false;
+    }
+};
