755945bd36cad24fd38d66a569fe212e644802c2
bhargav0628 <bhargav061028@gmail.com>
2026-09-22 22:42:07 +0530

    Time: 2 ms (43.65%), Space: 11.9 MB (17.98%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {\
+         vector<string> solution;
+        for(int i = 0;i<n;i++){
+            if((i+1)%15==0){
+               solution.push_back("FizzBuzz");
+            }
+            else if((i+1)%3==0){
+                solution.push_back("Fizz");
+            }
+            else if((i+1)%5==0){
+                solution.push_back("Buzz");
+            } else {
+                solution.push_back(to_string(i+1));
+            }
+        }
+        return solution;
+    }
+};
